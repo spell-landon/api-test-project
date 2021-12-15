c839bf5e40d9e023288fc59b27dfa51148f7fb70
spell-landon <spell.landon@gmail.com>
2021-12-15 12:58:40 -0600

    Add API functionality

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+const baseURL = `https://uselessfacts.jsph.pl/`;
+
+const buttonEl = document.querySelector('button');
+const pEl = document.querySelector('p');
+
+buttonEl.addEventListener('click', getFact);
+
+function getFact() {
+  pEl.textContent = '';
+  fetch(`${baseURL}/random.json?language=en`)
+    .then((res) => res.json())
+    .then((res) => {
+      pEl.textContent = res.text;
+    })
+    .catch((err) => {
+      console.error(`Something went wrong: `, err);
+    })
+    .finally(() => {
+      return;
+    });
+}
